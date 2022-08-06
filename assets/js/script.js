f46b9e21b954489bd6012e3cde6ccf64d19d9be0
Doanh Duc <doanhthangduc2802@gmail.com>
2022-08-06 22:59:19 +0700

    resolve bug, CSS, html as comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,47 @@
-
 // making close and open navigation
 
-const menuBtn = document.querySelector('.mobie__menuBtn');
-const openMobieNav = document.getElementsByClassName('my__link');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open');
-    if (openMobieNav[0].style.display === '') {
-        openMobieNav[0].style.display = 'flex';
-    } else {
-        openMobieNav[0].style.display = 'none';
+const menuBtn = $(".mobie__menuBtn");
+const openMobieNav = $(".my__link");
+const headingLinks = $$(".heading a");
+const headingIntro = $(".desktop.heading__intro");
+const headingAbout = $(".desktop.heading__about");
+const headingWorks = $(".desktop.heading__works");
+const headingSayHello = $(".desktop.heading__say_hello");
+
+const app = {
+  // handle events
+  handleEvent: function () {
+    // handle navigation scrollY events
+    window.onscroll = function () {
+      let scrollValue =
+        window.scrollY || document.documentElement.scrollTop || 0;
+      if (scrollValue <= 772) {
+        headingIntro.classList.add("scrollToHeading");
+      } else {
+        headingIntro.classList.remove("scrollToHeading");
+      }
+      if (scrollValue > 772 && scrollValue <= 3269) {
+        headingAbout.classList.add("scrollToHeading");
+      } else {
+        headingAbout.classList.remove("scrollToHeading");
+      }
+      if (scrollValue > 3269 && scrollValue <= 4550) {
+        headingWorks.classList.add("scrollToHeading");
+      } else {
+        headingWorks.classList.remove("scrollToHeading");
+      }
+      if (scrollValue > 4550) {
+        headingSayHello.classList.add("scrollToHeading");
+      } else {
+        headingSayHello.classList.remove("scrollToHeading");
+      }
     };
-});
+  },
+  start: function () {
+    this.handleEvent();
+  },
+};
+app.start();
